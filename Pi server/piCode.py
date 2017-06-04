93afdd25ef9c67475059e47d52557bf5c39c2dfe
Danny Roberts <Danny Roberts>
2017-06-04 14:37:33 +0100

    bugfix #2 - indentation

--- Pi server/piCode.py
+++ Pi server/piCode.py
@@ -74,15 +74,15 @@ class SubscriptionThread(Thread):
 				# perform action based on JSON request
 				if action == 'make_drink':
 					make_drink(json_obj)
-          print("Making drink")
+					print("Making drink")
 					#Do some other flashy thing
 				elif action == 'flash_unavailable':
-          print("flash_unavailable")
-          self.msg_queue.put('xu!') #Hotword
+					print("flash_unavailable")
+					self.msg_queue.put('xu!') #Hotword
 					#Do the flashy thing
 				elif action == 'flash_busy':
-          print("flash_busy")
-          self.msg_queue.put('xu!') #Hotword
+					print("flash_busy")
+					self.msg_queue.put('xu!') #Hotword
 					#Do the flashy thing
 			# ack received message
 			if results:
@@ -93,17 +93,17 @@ class SubscriptionThread(Thread):
 
 class SerialThread(Thread):
 
-  def __init__(self, msg_queue):
+	def __init__(self, msg_queue):
 	Thread.__init__(self)
 	self.shutdown_flag = Event()
 	self.msg_queue = msg_queue;
 	self.serial = serial.Serial(SER_DEVICE, 9600)
 
-  def run(self):
+	def run(self):
 
 	while not self.shutdown_flag.is_set():
 
-	  if not self.msg_queue.empty():
+		if not self.msg_queue.empty():
 		cmd = self.msg_queue.get()
 		self.serial.write(str.encode(cmd))
 		print('Serial sending ' + cmd)
@@ -111,145 +111,145 @@ class SerialThread(Thread):
 
 def setup_assistant():
 
-    # Load credentials.
-    try:
-      credentials = os.path.join(
-          click.get_app_dir(common_settings.ASSISTANT_APP_NAME),
-          common_settings.ASSISTANT_CREDENTIALS_FILENAME
-      )
-      global creds
-      creds = auth_helpers.load_credentials(credentials, scopes=[common_settings.ASSISTANT_OAUTH_SCOPE, common_settings.PUBSUB_OAUTH_SCOPE])
-    except Exception as e:
-      logging.error('Error loading credentials: %s', e)
-      logging.error('Run auth_helpers to initialize new OAuth2 credentials.')
-      return -1
+		# Load credentials.
+		try:
+			credentials = os.path.join(
+					click.get_app_dir(common_settings.ASSISTANT_APP_NAME),
+					common_settings.ASSISTANT_CREDENTIALS_FILENAME
+			)
+			global creds
+			creds = auth_helpers.load_credentials(credentials, scopes=[common_settings.ASSISTANT_OAUTH_SCOPE, common_settings.PUBSUB_OAUTH_SCOPE])
+		except Exception as e:
+			logging.error('Error loading credentials: %s', e)
+			logging.error('Run auth_helpers to initialize new OAuth2 credentials.')
+			return -1
 
-    # Create gRPC channel
-    grpc_channel = auth_helpers.create_grpc_channel(ASSISTANT_API_ENDPOINT, creds)
-    logging.info('Connecting to %s', ASSISTANT_API_ENDPOINT)
+		# Create gRPC channel
+		grpc_channel = auth_helpers.create_grpc_channel(ASSISTANT_API_ENDPOINT, creds)
+		logging.info('Connecting to %s', ASSISTANT_API_ENDPOINT)
 
-    # Create Google Assistant API gRPC client.
-    global assistant
-    assistant = embedded_assistant_pb2.EmbeddedAssistantStub(grpc_channel)
-    return 0
+		# Create Google Assistant API gRPC client.
+		global assistant
+		assistant = embedded_assistant_pb2.EmbeddedAssistantStub(grpc_channel)
+		return 0
  
 class AssistantThread(Thread):
 
-  def __init__(self, msg_queue):
-    Thread.__init__(self)
-    self.shutdown_flag = Event()
-    self.button_flag = Event()
-    self.msg_queue = msg_queue
+	def __init__(self, msg_queue):
+		Thread.__init__(self)
+		self.shutdown_flag = Event()
+		self.button_flag = Event()
+		self.msg_queue = msg_queue
 
-  def run(self):
+	def run(self):
 
-    # Configure audio source and sink.
-    audio_device = None
-    audio_source = audio_device = (
-        audio_device or audio_helpers.SoundDeviceStream(
-            sample_rate=common_settings.DEFAULT_AUDIO_SAMPLE_RATE,
-            sample_width=common_settings.DEFAULT_AUDIO_SAMPLE_WIDTH,
-            block_size=common_settings.DEFAULT_AUDIO_DEVICE_BLOCK_SIZE,
-            flush_size=common_settings.DEFAULT_AUDIO_DEVICE_FLUSH_SIZE
-        )
-    )
-    audio_sink = audio_device = (
-        audio_device or audio_helpers.SoundDeviceStream(
-            sample_rate=common_settings.DEFAULT_AUDIO_SAMPLE_RATE,
-            sample_width=common_settings.DEFAULT_AUDIO_SAMPLE_WIDTH,
-            block_size=common_settings.DEFAULT_AUDIO_DEVICE_BLOCK_SIZE,
-            flush_size=common_settings.DEFAULT_AUDIO_DEVICE_FLUSH_SIZE
-        )
-    )
+		# Configure audio source and sink.
+		audio_device = None
+		audio_source = audio_device = (
+				audio_device or audio_helpers.SoundDeviceStream(
+						sample_rate=common_settings.DEFAULT_AUDIO_SAMPLE_RATE,
+						sample_width=common_settings.DEFAULT_AUDIO_SAMPLE_WIDTH,
+						block_size=common_settings.DEFAULT_AUDIO_DEVICE_BLOCK_SIZE,
+						flush_size=common_settings.DEFAULT_AUDIO_DEVICE_FLUSH_SIZE
+				)
+		)
+		audio_sink = audio_device = (
+				audio_device or audio_helpers.SoundDeviceStream(
+						sample_rate=common_settings.DEFAULT_AUDIO_SAMPLE_RATE,
+						sample_width=common_settings.DEFAULT_AUDIO_SAMPLE_WIDTH,
+						block_size=common_settings.DEFAULT_AUDIO_DEVICE_BLOCK_SIZE,
+						flush_size=common_settings.DEFAULT_AUDIO_DEVICE_FLUSH_SIZE
+				)
+		)
 
-    # Create conversation stream with the given audio source and sink.
-    conversation_stream = audio_helpers.ConversationStream(
-        source=audio_source,
-        sink=audio_sink,
-        iter_size=common_settings.DEFAULT_AUDIO_ITER_SIZE,
-    )
+		# Create conversation stream with the given audio source and sink.
+		conversation_stream = audio_helpers.ConversationStream(
+				source=audio_source,
+				sink=audio_sink,
+				iter_size=common_settings.DEFAULT_AUDIO_ITER_SIZE,
+		)
 
-    conversation_state_bytes = None
-    volume_percentage = 50
-    follow_on = True
+		conversation_state_bytes = None
+		volume_percentage = 50
+		follow_on = True
 
-    while not self.shutdown_flag.is_set():
+		while not self.shutdown_flag.is_set():
 
-      # conversation ux lights off
-      self.msg_queue.put('xo!')
+			# conversation ux lights off
+			self.msg_queue.put('xo!')
 
-      # get manual input start
-      if not follow_on:
+			# get manual input start
+			if not follow_on:
 
-        if PUSH_TO_TALK:
-          while not self.button_flag.is_set():
-            time.sleep(0.1)
-          self.button_flag.clear()
-        else :
-          print('Press Enter to send a new request.')
-          input()
+				if PUSH_TO_TALK:
+					while not self.button_flag.is_set():
+						time.sleep(0.1)
+					self.button_flag.clear()
+				else :
+					print('Press Enter to send a new request.')
+					input()
 
-        self.msg_queue.put('xh!') #Hotword
+				self.msg_queue.put('xh!') #Hotword
 
-      else:
-        self.msg_queue.put('xl!') #Listening
+			else:
+				self.msg_queue.put('xl!') #Listening
 
-      conversation_stream.start_recording()
-      logging.info('Recording audio request.')
+			conversation_stream.start_recording()
+			logging.info('Recording audio request.')
 
-      # This generator yields ConverseRequest to send to the gRPC Google Assistant API.
-      converse_requests = assistant_helpers.gen_converse_requests(
-          conversation_stream,
-          sample_rate=common_settings.DEFAULT_AUDIO_SAMPLE_RATE,
-          conversation_state=conversation_state_bytes,
-          volume_percentage=volume_percentage
-      )
+			# This generator yields ConverseRequest to send to the gRPC Google Assistant API.
+			converse_requests = assistant_helpers.gen_converse_requests(
+					conversation_stream,
+					sample_rate=common_settings.DEFAULT_AUDIO_SAMPLE_RATE,
+					conversation_state=conversation_state_bytes,
+					volume_percentage=volume_percentage
+			)
 
-      def iter_converse_requests():
-        for c in converse_requests:
-          assistant_helpers.log_converse_request_without_audio(c)
-          yield c
-        conversation_stream.start_playback()
+			def iter_converse_requests():
+				for c in converse_requests:
+					assistant_helpers.log_converse_request_without_audio(c)
+					yield c
+				conversation_stream.start_playback()
 
-      # This generator yields ConverseResponse proto messages received from the gRPC Google Assistant API.
-      for resp in assistant.Converse(iter_converse_requests(), common_settings.DEFAULT_GRPC_DEADLINE):
+			# This generator yields ConverseResponse proto messages received from the gRPC Google Assistant API.
+			for resp in assistant.Converse(iter_converse_requests(), common_settings.DEFAULT_GRPC_DEADLINE):
 
-        assistant_helpers.log_converse_response_without_audio(resp)
+				assistant_helpers.log_converse_response_without_audio(resp)
 
-        if resp.error.code != code_pb2.OK:
-          logging.error('server error: %s', resp.error.message)
-          break
+				if resp.error.code != code_pb2.OK:
+					logging.error('server error: %s', resp.error.message)
+					break
 
-        if resp.event_type == END_OF_UTTERANCE:
-          logging.info('End of audio request detected')
-          conversation_stream.stop_recording()
-          self.msg_queue.put('xt!') # conversation ux lights thinking
+				if resp.event_type == END_OF_UTTERANCE:
+					logging.info('End of audio request detected')
+					conversation_stream.stop_recording()
+					self.msg_queue.put('xt!') # conversation ux lights thinking
 
-        if resp.result.spoken_request_text:
-          logging.info('Transcript of user request: "%s".', resp.result.spoken_request_text)
-          logging.info('Playing assistant response.')
-          self.msg_queue.put('xr!') # conversation ux lights responding
+				if resp.result.spoken_request_text:
+					logging.info('Transcript of user request: "%s".', resp.result.spoken_request_text)
+					logging.info('Playing assistant response.')
+					self.msg_queue.put('xr!') # conversation ux lights responding
 
-        if len(resp.audio_out.audio_data) > 0:
-          # print('writing audio data')
-          conversation_stream.write(resp.audio_out.audio_data)
+				if len(resp.audio_out.audio_data) > 0:
+					# print('writing audio data')
+					conversation_stream.write(resp.audio_out.audio_data)
 
-        if resp.result.conversation_state:
-          conversation_state_bytes = resp.result.conversation_state
+				if resp.result.conversation_state:
+					conversation_state_bytes = resp.result.conversation_state
 
-        if resp.result.volume_percentage != volume_percentage:
-          volume_percentage = resp.result.volume_percentage
-          logging.info('Volume should be set to %s%%' % volume_percentage)
+				if resp.result.volume_percentage != volume_percentage:
+					volume_percentage = resp.result.volume_percentage
+					logging.info('Volume should be set to %s%%' % volume_percentage)
 
-        # check for follow on
-        if resp.result.microphone_mode == DIALOG_FOLLOW_ON:
-          follow_on = True
-          logging.info('Expecting follow-on query from user.')
-        elif resp.result.microphone_mode == CLOSE_MICROPHONE:
-          follow_on = False
-          logging.info('Not expecting follow-on query from user.')
+				# check for follow on
+				if resp.result.microphone_mode == DIALOG_FOLLOW_ON:
+					follow_on = True
+					logging.info('Expecting follow-on query from user.')
+				elif resp.result.microphone_mode == CLOSE_MICROPHONE:
+					follow_on = False
+					logging.info('Not expecting follow-on query from user.')
 
-    conversation_stream.close()
+		conversation_stream.close()
 
 
 
